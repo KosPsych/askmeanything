2ea7cee866254b193c10bb879a1ec19d7da4193c
johnlyras98 <71226053+johnlyras98@users.noreply.github.com>
2021-04-19 17:21:33 +0300

    Add files via upload

--- front-end/src/MyContributions.js
+++ front-end/src/MyContributions.js
@@ -10,55 +10,6 @@ class MyContributions extends React.Component {
     };
   }
 
-  componentDidMount() {
-    const requestOptions = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-OBSERVATORY-AUTH': localStorage.getItem("token")
-      }
-    }
-    fetch('//localhost:8765/evcharge/api/UserReport/' + localStorage.getItem('username') + '/' + localStorage.getItem("fromdate") + '/' + localStorage.getItem("todate"), requestOptions)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        $(this.refs.main).DataTable({
-          data: data.CarSummary,
-          columns: [
-            {
-              title: 'Brand name',
-              width: "15%",
-              data: 'BrandName'
-            },
-            {
-              title: 'Model',
-              width: "15%",
-              data: 'Model'
-            },
-            {
-              title: 'Cost',
-              width: "15%",
-              data: 'Cost'
-            },
-            {
-              title: 'Energy delivered',
-              width: "15%",
-              data: 'EnergyDelivered'
-            },
-            {
-              title: 'Sessions',
-              width: "15%",
-              data: 'Sessions'
-            }
-          ],
-          ordering: false
-        });
-      })
-      .catch(error => {
-        console.error(error);
-      })
-  }
   render() {
     return (
       <html>
